f74cc6f83f4eb961dbb6089e8f97cf1dddc75fc7
Stef <stefano.delprete.08@gmail.com>
2021-07-27 17:24:15 +0200

    Update buttonsPagination.js

--- src/buttonsPagination.js
+++ src/buttonsPagination.js
@@ -1,2 +1,22 @@
 module.exports = {
-  function NAME(message, options = {}) {
+  async function NAME(message, options = {}) {
+    if (!message) {
+      throw new Error('Discord.js Pagination Error: message argument wad not specified')
+    }
+    if (typeof message !== 'object') {
+      throw new TypeError('Discord.js Pagination Error: message must be an object')
+    }
+    let nextButtonColor = 'SUCCESS';
+    let previousButtonColor = 'DANGER';
+    if (options.nextButtonColor) {
+      if (typeof options.nextButtonColor !== 'string) {
+        throw new TypeError('Discord.js Pagination Error: nextButtonColor argument must be a string')
+      }
+      const possibilities = ["DANGER", "SUCCESS", "PRIMARY", "SECONDARY"];
+      if (!possibilities[options.nextButtonColor]) {
+        throw new TypeError('Discord.js Pagination Error: nextButtonColor is not a valid style (avaible styles are SUCCESS, PRIMARY, SECONDARY and DANGER)')
+      }
+      if (possibilities[options.nextButtonColor]) {
+        nextButtonColor = options.nextButtonColor;
+      }
+    }
